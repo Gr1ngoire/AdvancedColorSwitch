b49944496b272f795e523bbe3b8b2f5c6122ef81
Gr1ngoire <gringoirelamusicart@gmail.com>
2021-04-25 19:15:57 +0300

    Second step os complete( custom protocol left )

--- HW7_Course2/SwitchColorController.swift
+++ HW7_Course2/SwitchColorController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SwitchColorController: UIViewController {
+class SwitchColorController: UIViewController, UITextFieldDelegate {
 
     // color View
     @IBOutlet weak var colorOutput: UIView!
@@ -29,16 +29,32 @@ class SwitchColorController: UIViewController {
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
     
-    
     // segue color from previous screen
     var viewColor: UIColor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // tap recognizer
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+        
+        // editing the view
         colorOutput.layer.cornerRadius = colorOutput.frame.width / 40
         colorOutput.backgroundColor = viewColor
+        
+        // adding done button for text fields
+        redTextField.addDoneButtonOnKeyboard()
+        greenTextField.addDoneButtonOnKeyboard()
+        blueTextField.addDoneButtonOnKeyboard()
+        
+        self.redTextField.delegate = self
+        self.greenTextField.delegate = self
+        self.blueTextField.delegate = self
     }
+    
     
 
     @IBAction func doneButtonPressed(_ sender: UIButton) {
@@ -64,6 +80,30 @@ class SwitchColorController: UIViewController {
     }
     
     
+    // done button action
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let value = textField.text else { return }
+        
+        if let convertedValue = Float(value) {
+            
+            switch textField.tag {
+            case 0:
+                redSlider.value = convertedValue
+            case 1:
+                greenSlider.value = convertedValue
+            case 2:
+                blueSlider.value = convertedValue
+            default:
+                break
+            }
+            
+        } else {
+            let alert = UIAlertController(title: "Ooops!", message: "Please, enter proper format", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     
 }
 extension SwitchColorController {
@@ -79,4 +119,34 @@ extension SwitchColorController {
                                        blue: CGFloat(sliderThree.value),
                                        alpha: 1.0)
     }
+}
+
+extension SwitchColorController {
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
+
+
+extension UITextField {
+    func addDoneButtonOnKeyboard() {
+        let doneToolBar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width,  height: 50))
+        doneToolBar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action:
+                                                        #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolBar.items = items
+        doneToolBar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolBar
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+    
 }
